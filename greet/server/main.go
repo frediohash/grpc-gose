a84fdf83bdee79fe8ea6ac175f9808cadafd733a
ona mel <onamel313@gmail.com>
2022-04-26 00:05:14 +0700

    update

---
+++ greet/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+	log.Printf("Listening on %s\n", addr)
+}
